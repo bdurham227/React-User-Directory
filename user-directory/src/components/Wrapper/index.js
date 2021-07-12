9fac1a79e2ee638258054d57019f510144c283e5
bdurham227 <bdurham227@gmail.com>
2021-07-12 14:59:42 -0400

    finished building out wrapper page

--- user-directory/src/components/Wrapper/index.js
+++ user-directory/src/components/Wrapper/index.js
@@ -42,7 +42,7 @@ filterUsers() {
 
 renderUsers = () => {
   return this.filterUsers()
-  .sort(this.sortUsers)
+  .sort(this.arrangeUsers)
   .map((user, index) => {
     return (
       <tr key={index}>
@@ -52,16 +52,96 @@ renderUsers = () => {
         <td>{user.first}</td>
         <td>{user.last}</td>
         <td>{user.email}</td>
+        <td>{new Date(user.dob).toDateString()}</td>
       </tr>
      
-    )
-  })
+    );
+  });
+};
+
+handleHeaderClass = col => {
+  return this.state.col === col ? `clickable ${this.state.sort}` : `clickable`;
+};
+
+handleArrangeFormat = col => {
+  this.state.col === col && this.state.sort === "ascending"
+      ? this.setState({ sort: "descending", col: col })
+      : this.setState({ sort: "ascending", col: col });
 }
 
 
 
+arrangeUsers = (a, b) => {
+  if(a[this.state.col] < b[this.state.col]) {
+    return this.state.sort === 'ascending' ? -1 : 1;
+  } else if (a[this.state.col] > b[this.state.col]) {
+    return this.state.sort === 'ascending' ? 1: -1;
+  }
+  return 0;
+};
+
+
+
   render() { 
-    return (  );
+    return ( 
+      <>
+      <div className="input-group justify-content-center">
+        <div className="input-group-prepend"></div>
+        <input
+          onChange={this.handleInputChange}
+          type="search"
+          className="form-control m-3"
+          placeholder="Search"
+          aria-label="SearchBox"
+          aria-describedby="basic-addon1"
+        />
+      </div>
+      <div className="table m-3">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Image</th>
+              <th scope="col">
+                <span
+                  className={this.handleHeaderClass("first")}
+                  onClick={() => {
+                    this.handleArrangeFormat("first");
+                  }}
+                >
+                  First
+                </span>
+              </th>
+              <th scope="col">
+                <span
+                  className={this.handleHeaderClass("last")}
+                  onClick={() => this.handleArrangeFormat("last")}
+                >
+                  Last
+                </span>
+              </th>
+              <th scope="col">
+                <span
+                  className={this.handleHeaderClass("email")}
+                  onClick={() => this.handleArrangeFormat("email")}
+                >
+                  Email
+                </span>
+              </th>
+              <th scope="col">
+                <span
+                  className={this.handleHeaderClass("dob")}
+                  onClick={() => this.handleArrangeFormat("dob")}
+                >
+                  DOB
+                </span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>{this.renderUsers()}</tbody>
+        </table>
+      </div>
+    </>
+     );
   }
 }
  
